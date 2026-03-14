27b1db1e5a7f5b13479bec6dfb03d5186d4f157d
litlighilit <litlighilit@foxmail.com>
2026-03-14 15:47:00 +0800

    feat,fixup: short_repr

--- float_utils.nimble
+++ float_utils.nimble
@@ -10,3 +10,17 @@ srcDir        = "src"
 # Dependencies
 
 requires "nim >= 2.0.10"
+
+var pylibPre = "https://github.com/nimpylib"
+let envVal = getEnv("NIMPYLIB_PKGS_BARE_PREFIX")
+if envVal != "": pylibPre = ""
+#if pylibPre == Def: pylibPre = ""
+elif pylibPre[^1] != '/':
+  pylibPre.add '/'
+template pylib(x, ver) =
+  requires if pylibPre == "": x & ver
+           else: pylibPre & x
+pylib "dtoa_c", " ^= 0.1.0"
+pylib "autoconf_sugars", " ^= 0.1.0"
+pylib "errno", " ^= 0.1.0"
+
--- src/float_utils/round/PY_SHORT_FLOAT_REPR.nim
+++ src/float_utils/round/PY_SHORT_FLOAT_REPR.nim
@@ -1,9 +1,7 @@
 
-import ./reimporter
-
-import_pyconfig
-import_dtoa
-import_obmalloc
+import pkg/dtoa_c
+import pkg/errno/[errnoUtils, errnoConsts]
+import ./err
 
 proc PyOS_snprintf(str: cstring, size: csize_t|int): cint{.discardable, importc: "snprintf", header: "<stdio.h>", cdecl, varargs.}
 # Include/internal/pycore_pymath.h
@@ -11,55 +9,52 @@ proc PyOS_snprintf(str: cstring, size: csize_t|int): cint{.discardable, importc:
 proc round*(dd: float, ndigits: int): float =
   ##[ version of double_round that uses the correctly-rounded string<->double
     conversions from Python/dtoa.c ]##
-  const mode = 3
   const MyBufLen = 100
   var
     mybuflen = MyBufLen
     buf, buf_end: cstring
     shortbuf: array[MyBufLen, cchar]
     mybuf: cstring = cast[cstring](addr shortbuf[0])
-    decpt, sign: c_int
-
-  Py_SET_53BIT_PRECISION_HEADER
+    decpt: c_int
+    sign: bool
 
   # round to a decimal string
 
-  Py_SET_53BIT_PRECISION_START
-  buf = dtoa(dd.cdouble, mode.cint, ndigits.cint, decpt, sign, buf_end)
-  Py_SET_53BIT_PRECISION_END
-  template preexit =
-    freedtoa buf
+  buf = dtoa(dd.cdouble, DTOA_DECIMAL, ndigits.cint, decpt, sign, buf_end)
+  defer: freedtoa buf
+
   template chkNoMem(p: ptr|cstring) =
     if p.isNil:
-      preexit
       raise newException(OutOfMemDefect, "")
   buf.chkNoMem
 
   #[Get new buffer if shortbuf is too small.  Space needed <= buf_end -
     buf + 8: (1 extra for '0', 1 for sign, 5 for exp, 1 for '\0').]#
   let buflen = cast[int](buf_end) - cast[int](buf)
+
+  assert buf_end.isNil.not
+  assert buflen >= 0
   if buflen  + 8 > mybuflen:
     mybuflen = buflen + 8
-    mybuf = pyallocStr mybuflen
+    mybuf = cast[cstring](alloc mybuflen)
     mybuf.chkNoMem
 
   # copy buf to mybuf, adding exponent, sign and leading 0
   PyOS_snprintf(mybuf, mybuflen, "%s0%se%d",
-                (if sign.bool: cstring"-" else: cstring""),
-                buf, cast[cint](decpt) - buflen.cint)
+                (if sign: cstring"-" else: cstring""),
+                buf, decpt - buflen.cint)
 
   # and convert the resulting string back to a double
   prepareRWErrno
   setErrno0
-  Py_SET_53BIT_PRECISION_START
-  result = strtod(mybuf, nil)
-  Py_SET_53BIT_PRECISION_END
+
+  result = strtod(mybuf)
+
 
   if isErr(ERANGE) and abs(result) >= 1:
-    raise newException(OverflowDefect, "rounded value too large to represent")
+    raise newException(err.OverflowError, "rounded value too large to represent")
 
   # done computin value
   if cast[ptr cchar](mybuf) != shortbuf[0].addr:
-    pyfreeStr mybuf
+    dealloc mybuf
 
-  preexit
